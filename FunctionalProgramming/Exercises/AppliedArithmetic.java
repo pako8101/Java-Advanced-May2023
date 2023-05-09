c5a0df0757db2e8ff905a7e36c37ce7d5c047c4e
Plamen Kamenov <112328223+pako8101@users.noreply.github.com>
2023-05-09 12:58:52 +0300

    Add files via upload

--- FunctionalProgramming/Exercises/AppliedArithmetic.java
+++ FunctionalProgramming/Exercises/AppliedArithmetic.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 
@@ -14,6 +15,7 @@ public class AppliedArithmetic {
                 .map(Integer::parseInt).collect(Collectors.toList());
 //        Consumer<List<Integer>> kany = numbers-> numbers.replaceAll(e->e+1);
         UnaryOperator<List<Integer>> add = numbers -> numbers.stream().map(number -> number + 1).collect(Collectors.toList());
+        Function<List<Integer>, List<Integer>> addCommand = listFun -> listFun.stream().map(n->n +=1).collect(Collectors.toList());
         UnaryOperator<List<Integer>> subtract = numbers -> numbers.stream().map(number -> number - 1).collect(Collectors.toList());
         UnaryOperator<List<Integer>> multiply = numbers -> numbers.stream().map(number -> number * 2).collect(Collectors.toList());
         Consumer<List<Integer>> printer = numbers -> numbers.forEach(number -> System.out.print(number + " "));
@@ -21,7 +23,7 @@ public class AppliedArithmetic {
         while (!command.equals("End")) {
             switch (command) {
                 case "add":
-                    list = add.apply(list);
+                    list = addCommand.apply(list);
                     break;
                 case "subtract":
                     list = subtract.apply(list);
