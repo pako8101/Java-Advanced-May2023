929aba94b350452ffa95a6ccfc0ac1261a5d985d
Plamen Kamenov <112328223+pako8101@users.noreply.github.com>
2023-05-10 19:23:50 +0300

    Add files via upload

--- IteratorsAndComparators/Exercises/ListyIterator/Main.java
+++ IteratorsAndComparators/Exercises/ListyIterator/Main.java
@@ -1,18 +1,23 @@
 package IteratorsAndComparators.Exercises.ListyIterator;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         Scanner scanner = new Scanner(System.in);
+//        String createInput = scanner.nextLine();
+//        List<String> data = Arrays.stream(createInput.split(" "))
+//                .skip(1).collect(Collectors.toList());
+//        ListyIterator listyIterator = new ListyIterator(data);
         String input = scanner.nextLine();
         ListyIterator listyIterator = null;
         while (!input.equals("END")) {
             String[] commandPArts = input.split(" ");
             String command = commandPArts[0];
             switch (command) {
-
                 case "Create":
                     if (commandPArts.length > 1) {
                         String[] elementArray = Arrays.copyOfRange(commandPArts, 1, commandPArts.length);
