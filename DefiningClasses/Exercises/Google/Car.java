f84974002d4a912bcce5d1c569a671cd3592f1c0
Plamen Kamenov <112328223+pako8101@users.noreply.github.com>
2023-05-21 13:45:36 +0300

    Add files via upload

--- DefiningClasses/Exercises/Google/Car.java
+++ DefiningClasses/Exercises/Google/Car.java
@@ -11,6 +11,6 @@ public class Car {
 
     @Override
     public String toString() {
-        return carModel + " " + String.format("%d", carSpeed);
+        return carModel + " " +  carSpeed;
     }
 }
